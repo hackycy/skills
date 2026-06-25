8d4b48911e896cfffd607521ad4d7345ad65ec92
Changyuan Yang <26972260+hackycy@users.noreply.github.com>
2026-06-25 22:09:03 +0800

    chore(meta): remove unused instructions and update README

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Preferences agent skills
 ## Installation
 
 ```bash
-pnpx skills add hackycy-collection/skills
+pnpx skills add hackycy/skills
 ```
 
 Learn more about the CLI usage at [skills](https://github.com/vercel-labs/skills).
@@ -13,7 +13,17 @@ Learn more about the CLI usage at [skills](https://github.com/vercel-labs/skills
 ## Inspire
 
 - https://github.com/antfu/skills
-- https://github.com/pbakaus/impeccable
+
+## Awesome Skills
+
+- [superpowers](https://github.com/obra/superpowers)
+- [baoyu-skills](https://github.com/JimLiu/baoyu-skills)
+- [impeccable](https://github.com/pbakaus/impeccable)
+- [awesome-design-md](https://github.com/VoltAgent/awesome-design-md)
+
+## Links
+
+- https://awesome-copilot.github.com/
 
 ## License
 
--- meta.ts
+++ meta.ts
@@ -1,7 +1,12 @@
 export interface VendorSkillMeta {
-  official?: boolean
+  /**
+   * git source
+   */
   source: string
-  skills: Record<string, string> // sourceSkillName -> outputSkillName
+  /**
+   * sourceSkillName -> outputSkillName
+   */
+  skills: Record<string, string>
 }
 
 /**
@@ -9,21 +14,18 @@ export interface VendorSkillMeta {
  */
 export const vendors: Record<string, VendorSkillMeta> = {
   vueuse: {
-    official: true,
     source: 'https://github.com/vueuse/vueuse',
     skills: {
       'vueuse-functions': 'vueuse-functions',
     },
   },
   tsdown: {
-    official: true,
     source: 'https://github.com/rolldown/tsdown',
     skills: {
       tsdown: 'tsdown',
     },
   },
   turborepo: {
-    official: true,
     source: 'https://github.com/vercel/turborepo',
     skills: {
       turborepo: 'turborepo',
